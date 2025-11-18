febb3f89d495cf009c7cb21990052e93eb11927c
BOUCHAIB EL HAIDI <bouchaibelhaidi1@gmail.com>
2025-11-18 15:26:59 +0100

    adding the design for Desktop

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        staffGreen: '#38A747'
+      }
+    }
+  },
+  plugins: [],
+}
